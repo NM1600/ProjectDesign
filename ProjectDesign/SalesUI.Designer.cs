2902134242f8539bd7aea6c76604a2864d56e320
NM1600 <mercadonestleann@gmail.com>
2022-12-11 21:12:31 +0800

    sales updated button (edit, delete)

--- ProjectDesign/SalesUI.Designer.cs
+++ ProjectDesign/SalesUI.Designer.cs
@@ -30,20 +30,22 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.deletebtn = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnSalesTransaction = new System.Windows.Forms.Button();
             this.panel6 = new System.Windows.Forms.Panel();
             this.EditBtn = new System.Windows.Forms.Button();
-            this.btnSalesTransaction = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,8 +56,9 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1883, 639);
+            this.panel1.Size = new System.Drawing.Size(1924, 786);
             this.panel1.TabIndex = 2;
             // 
             // panel3
@@ -63,31 +66,11 @@
             this.panel3.Controls.Add(this.dataGridView1);
             this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(0, 62);
+            this.panel3.Location = new System.Drawing.Point(0, 76);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1883, 577);
+            this.panel3.Size = new System.Drawing.Size(1924, 710);
             this.panel3.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(226)))), ((int)(((byte)(237)))));
-            this.panel2.Controls.Add(this.panel5);
-            this.panel2.Controls.Add(this.panel4);
-            this.panel2.Controls.Add(this.panel6);
-            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1883, 62);
-            this.panel2.TabIndex = 0;
-            // 
-            // panel4
-            // 
-            this.panel4.Controls.Add(this.btnSalesTransaction);
-            this.panel4.Location = new System.Drawing.Point(8, 7);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(251, 52);
-            this.panel4.TabIndex = 0;
             // 
             // dataGridView1
             // 
@@ -95,9 +78,27 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1883, 577);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1924, 710);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(226)))), ((int)(((byte)(237)))));
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1924, 76);
+            this.panel2.TabIndex = 0;
             // 
             // panel5
             // 
@@ -106,9 +107,10 @@
             this.panel5.Controls.Add(this.deletebtn);
             this.panel5.Cursor = System.Windows.Forms.Cursors.Hand;
             this.panel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel5.Location = new System.Drawing.Point(1754, 9);
+            this.panel5.Location = new System.Drawing.Point(2339, 11);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(91, 39);
+            this.panel5.Size = new System.Drawing.Size(121, 48);
             this.panel5.TabIndex = 6;
             // 
             // deletebtn
@@ -119,12 +121,38 @@
             this.deletebtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
             this.deletebtn.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
             this.deletebtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.deletebtn.Location = new System.Drawing.Point(-13, -2);
+            this.deletebtn.Location = new System.Drawing.Point(-17, -2);
+            this.deletebtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.deletebtn.Name = "deletebtn";
-            this.deletebtn.Size = new System.Drawing.Size(118, 46);
+            this.deletebtn.Size = new System.Drawing.Size(157, 57);
             this.deletebtn.TabIndex = 1;
             this.deletebtn.Text = "   Delete";
             this.deletebtn.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnSalesTransaction);
+            this.panel4.Location = new System.Drawing.Point(11, 9);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(335, 64);
+            this.panel4.TabIndex = 0;
+            // 
+            // btnSalesTransaction
+            // 
+            this.btnSalesTransaction.BackColor = System.Drawing.Color.Transparent;
+            this.btnSalesTransaction.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalesTransaction.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalesTransaction.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
+            this.btnSalesTransaction.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalesTransaction.Location = new System.Drawing.Point(-99, -18);
+            this.btnSalesTransaction.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalesTransaction.Name = "btnSalesTransaction";
+            this.btnSalesTransaction.Size = new System.Drawing.Size(451, 87);
+            this.btnSalesTransaction.TabIndex = 1;
+            this.btnSalesTransaction.Text = "          Sales Transaction";
+            this.btnSalesTransaction.UseVisualStyleBackColor = false;
+            this.btnSalesTransaction.Click += new System.EventHandler(this.ButtonTransaction_Click);
             // 
             // panel6
             // 
@@ -133,9 +161,10 @@
             this.panel6.Controls.Add(this.EditBtn);
             this.panel6.Cursor = System.Windows.Forms.Cursors.Hand;
             this.panel6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel6.Location = new System.Drawing.Point(1673, 12);
+            this.panel6.Location = new System.Drawing.Point(2231, 15);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(91, 39);
+            this.panel6.Size = new System.Drawing.Size(121, 48);
             this.panel6.TabIndex = 4;
             // 
             // EditBtn
@@ -146,47 +175,66 @@
             this.EditBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
             this.EditBtn.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
             this.EditBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.EditBtn.Location = new System.Drawing.Point(-15, -5);
+            this.EditBtn.Location = new System.Drawing.Point(-20, -6);
+            this.EditBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.EditBtn.Name = "EditBtn";
-            this.EditBtn.Size = new System.Drawing.Size(119, 46);
+            this.EditBtn.Size = new System.Drawing.Size(159, 57);
             this.EditBtn.TabIndex = 1;
             this.EditBtn.Text = "Edit";
             this.EditBtn.UseVisualStyleBackColor = false;
             this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
             // 
-            // btnSalesTransaction
+            // button2
             // 
-            this.btnSalesTransaction.BackColor = System.Drawing.Color.Transparent;
-            this.btnSalesTransaction.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnSalesTransaction.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalesTransaction.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
-            this.btnSalesTransaction.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSalesTransaction.Location = new System.Drawing.Point(-74, -15);
-            this.btnSalesTransaction.Name = "btnSalesTransaction";
-            this.btnSalesTransaction.Size = new System.Drawing.Size(338, 71);
-            this.btnSalesTransaction.TabIndex = 1;
-            this.btnSalesTransaction.Text = "          Sales Transaction";
-            this.btnSalesTransaction.UseVisualStyleBackColor = false;
-            this.btnSalesTransaction.Click += new System.EventHandler(this.ButtonTransaction_Click);
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
+            this.button2.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.Location = new System.Drawing.Point(319, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 57);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
+            this.button3.Image = global::ProjectDesign.Properties.Resources.Add_icon1;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.Location = new System.Drawing.Point(486, 8);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 57);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "   Delete";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // SalesUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
-            this.ClientSize = new System.Drawing.Size(1883, 639);
+            this.ClientSize = new System.Drawing.Size(1924, 786);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SalesUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "SalesUI";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.panel6.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -204,5 +252,7 @@
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Button btnSalesTransaction;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
