54613f8fad462ee5ba38910e31e8af9afa52e127
JanLance <igotlance@outlook.com>
2022-12-11 15:25:39 +0800

    design sa sales UI and nag add window sa payment UI

--- ProjectDesign/SalesUI.Designer.cs
+++ ProjectDesign/SalesUI.Designer.cs
@@ -30,12 +30,18 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.ButtonTransaction = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(31)))), ((int)(((byte)(51)))));
+            this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -46,11 +52,40 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(226)))), ((int)(((byte)(237)))));
+            this.panel2.Controls.Add(this.panel4);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1454, 62);
             this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 62);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1454, 577);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.ButtonTransaction);
+            this.panel4.Location = new System.Drawing.Point(12, 5);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 52);
+            this.panel4.TabIndex = 0;
+            // 
+            // ButtonTransaction
+            // 
+            this.ButtonTransaction.BackColor = System.Drawing.Color.Transparent;
+            this.ButtonTransaction.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonTransaction.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonTransaction.Location = new System.Drawing.Point(-11, -10);
+            this.ButtonTransaction.Name = "ButtonTransaction";
+            this.ButtonTransaction.Size = new System.Drawing.Size(224, 71);
+            this.ButtonTransaction.TabIndex = 1;
+            this.ButtonTransaction.Text = "Sales Transaction";
+            this.ButtonTransaction.UseVisualStyleBackColor = false;
             // 
             // SalesUI
             // 
@@ -65,6 +100,8 @@
             this.Text = "SalesUI";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +110,8 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button ButtonTransaction;
     }
 }
